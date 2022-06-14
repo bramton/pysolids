9cb9d40eb8784102c9215b3685058cd698bf5e01
Bram <b.t.ton@saxion.nl>
2022-06-14 22:04:29 +0200

    Updated cylinder with area and volume

--- solids/Cylinder.py
+++ solids/Cylinder.py
@@ -10,13 +10,22 @@ class Cylinder(BaseSolid):
         self.ratio = ratio
 
     @property
-    def area(self):
+    def vol(self):
         return math.pi*self.r*self.r*self.ratio*self.r
+
+    @vol.setter
+    def vol(self, vol):
+        assert vol > 0, f"Volume should be positive, got: {vol}"
+        self.r = (vol/(math.pi*self.ratio)) ** (1/3)
+
+    @property
+    def area(self):
+        return 2*math.pi*self.r*self.r*(1 + self.ratio)
 
     @area.setter
     def area(self, area):
         assert area > 0, f"Area should be positive, got: {area}"
-        self.r = (area/(math.pi*self.ratio)) ** (1/3)
+        self.r = math.sqrt(area/(2*math.pi*(1 + self.ratio)))
 
     def SamplePoint(self, n=1024):
         assert n > 0, f"Number of points should be more than one, got: {n}"
